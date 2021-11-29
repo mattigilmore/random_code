7e11294059e89fbff517ed943ac6383325384094
Matti Gilmore <mattigilmore89@gmail.com>
2021-11-29 04:20:02 -0500

    Add files via upload

--- isItRaining.py
+++ isItRaining.py
@@ -1,31 +1,20 @@
-#yesno() is to make sure they only put in y or n
-yn=str(input("is it raining?"))
-yn=yn.lower()
 def outside():
-	print("Go out side")
-
-
-if yn== "yes" or "y":
-		yn=str(input("Do you have an umbrelle"))
-		yn=yn.lower()
-		if yn == "n" or "no":
-				endloop=True
-				while endloop==True:
-						yn=str(input(" wait till its not raining.\n Is it still raining?"))
-						print(yn)
-						yn=yn.lower()
-						print(yn)
-						if yn=="no" or "n":
-								outside()
-								endloop=False
-						else:
-								None
-								
-						
-
+    print("Go out side")
+    
+    
+answer = input("is it raining?").lower()
+if answer.startswith('y'):
+    answer = input("Do you have an umbrelle").lower()
+    if answer.startswith('n'):
+        while True:
+            answer = input(" wait till its not raining.\n Is it still raining?").lower()
+            print(answer)
+            if answer.startswith('n'):
+                outside()
+                break
 else:
-	outside()
-
+    outside()
+#cedit for  cleaning up and telling me about the start .startswith() goes to Aporetic on python discode.
 	
 		
 
